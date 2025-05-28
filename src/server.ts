86f7bc54e535815952dff38a19d610507337da48
csmanhaes86 <charlesmanhaes1@gmail.con>
2025-05-28 14:32:19 -0300

    istalando o prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello Express + TypeScript')
-})
+app.get("/", (request, response) => {
+  response.send("Hello Express + TypeScript");
+});
 
-app.listen(4000, ()=> console.log(`🚀 Server is running at port 4000!`))
+app.listen(4000, () => console.log("🚀 Server is running at port 4000!"));
