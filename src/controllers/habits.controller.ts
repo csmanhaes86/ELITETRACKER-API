5e1ff364dab4f8df62bb8641158c46575c7bec36
csmanhaes86 <charlesmanhaes1@gmail.con>
2025-06-01 21:14:21 -0300

    impedindo a criação dehábitos duplicados

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -6,6 +6,14 @@ export class HabitsController {
   store = async (request: Request, response: Response) => {
     const { name } = request.body;
 
+    const findHabit = await habitModel.findOne({
+      name,
+    });
+
+    if (findHabit) {
+      return response.status(400).json({ messege: "habith arleady exists." });
+    }
+
     const newHabits = await habitModel.create({
       name,
       completedDates: [],
